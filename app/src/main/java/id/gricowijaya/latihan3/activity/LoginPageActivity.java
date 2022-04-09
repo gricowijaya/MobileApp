78d9575e4648a87ca85a78b4d94c1773a081ab61
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-04-09 21:37:47 +0800

    update login form

---
+++ app/src/main/java/id/gricowijaya/latihan3/activity/LoginPageActivity.java
@@ -0,0 +1,19 @@
+package id.gricowijaya.latihan3.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuInflater;
+
+import id.gricowijaya.latihan3.R;
+
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
---
+++ app/src/main/java/id/gricowijaya/latihan3/model/LoginModel.java
@@ -0,0 +1,4 @@
+package id.gricowijaya.latihan3.model;
+
+public class LoginModel {
+}
